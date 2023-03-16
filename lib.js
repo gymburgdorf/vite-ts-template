0a68622daffb443c159b7cae0ff3e6078a229d6f
gymburgdorf <79353439+gymburgdorf@users.noreply.github.com>
2023-03-16 13:59:13 +0000

    Deploying to gh-pages from @ gymburgdorf/vite-ts-template@5633385b8d5dc89997e726f05daf972034a7beb5 🚀

---
+++ lib.js
@@ -0,0 +1,6 @@
+function o() {
+  console.log("lib working...");
+}
+export {
+  o as lib
+};
---
+++ lib.umd.cjs
@@ -0,0 +1,1 @@
+(function(e,i){typeof exports=="object"&&typeof module<"u"?i(exports):typeof define=="function"&&define.amd?define(["exports"],i):(e=typeof globalThis<"u"?globalThis:e||self,i(e.lib={}))})(this,function(e){"use strict";function i(){console.log("lib working...")}e.lib=i,Object.defineProperty(e,Symbol.toStringTag,{value:"Module"})});
